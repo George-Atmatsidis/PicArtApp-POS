b60cfa80556ac2080053420642c31f605c320515
xtrs84zk <xtrs84zk@gmail.com>
2019-11-05 15:24:31 -0600

    Almost enough, as me. Half-backed UserDTO, not yet ready for user pagination saveUser, updateUser, the usual stuff Half-backed user registration as "Admin" or "User" saveUser -> admin ; saveNonAdminUser -> user Update user now ciphers user password on store (always) might change it later.
    Signed-off-by: xtrs84zk <xtrs84zk@gmail.com>

--- src/main/java/com/chumbok/pos/controller/UserController.java
+++ src/main/java/com/chumbok/pos/controller/UserController.java
@@ -4,6 +4,7 @@ import com.chumbok.pos.dto.PersistedObjId;
 import com.chumbok.pos.dto.UserDTO;
 import com.chumbok.pos.entity.User;
 import com.chumbok.pos.service.UserService;
+import com.chumbok.pos.service.UserServiceLive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,24 +17,42 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
-
+    @Autowired
     private UserService userService;
 
     @RequestMapping(path = "/users", method = RequestMethod.GET)
-    public ModelAndView showAddStockForm(@RequestParam(required = false) Long userId) throws Exception {
+    public ModelAndView showAddStockForm(@RequestParam(required = false) Long userId, @Valid User user) throws Exception {
         ModelAndView modelAndView = new ModelAndView();
 
         if (userId != null) {
-
-            User temp = userService.getUser(userId);
             //TODO add the rest of this mf code
-           // modelAndView.addObject("userDTO", UserDTO);
+            modelAndView.addObject("user", userService.getUser(userId));
         } else {
-            modelAndView.addObject("userDTO", new UserDTO());
+            modelAndView.addObject("user", new User());
+            /*userService.updateUser(user);
+            modelAndView.addObject("userDTO", new UserDTO());*/
         }
 
-        modelAndView.setViewName("getUsers");
+        modelAndView.setViewName("user");
+        return modelAndView;
+    }
 
+    @RequestMapping(path = "/users", method = RequestMethod.POST)
+    public ModelAndView createUpdateUser(@RequestParam(value = "id", required = false) Long id, @Valid User user) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        if (id == null) {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "El usuario se ha registrado exitosamente.");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("product");
+        } else if (id != null) {
+            userService.updateUser(user);
+            modelAndView.addObject("successMessage", "Material actualizado correctamente.");
+
+            modelAndView.addObject("user", user);
+            modelAndView.setViewName("user");
+        }
         return modelAndView;
     }
    /* @Autowired
--- src/main/java/com/chumbok/pos/service/UserService.java
+++ src/main/java/com/chumbok/pos/service/UserService.java
@@ -1,15 +1,30 @@
 package com.chumbok.pos.service;
 
 import com.chumbok.pos.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface UserService {
     User findUserByEmail(String email);
+
     void saveUser(User user);
 
+    //I'm sorry, god
+    void saveNonAdminUser(User user);
+
+    //What have we done to get this shit
     void updateUser(User user);
+
+    //Aquí se pide el usuario, es un get; pretty straitghforward if you ask me, bro.
     User getUser(long id);
 
     List<User> getAllUsers();
+
+    //here's when it goes to shit
+    void disableUser(User user);
+
+    //this might actually work
+    Page<User> findAllByPage(Pageable pageable);
 }
